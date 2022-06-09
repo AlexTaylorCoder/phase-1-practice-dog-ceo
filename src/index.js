de9663d3cebb6d691f2246d52948270540614c6f
alextaylor <alextaylor515@gmail.com>
2022-06-09 13:50:43 -0400

    commit tree be0a8b905b5d3dfc9d490f760ffa58e0f993f0de

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('%c HI', 'color: firebrick')
+const dogContNode = document.querySelector("#dog-breeds")
+const dropDown = document.getElementById("breed-dropdown")
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+
+fetch(breedUrl).then(resp=>resp.json())
+.then(data =>{
+    for (let breed in data.message) {
+        //console.log(breed)
+        let subBreed 
+        data.message[breed].forEach(item => {
+            subBreed = item
+        });
+        const breedNode = document.createElement("li")
+        if (subBreed) {
+        breedNode.textContent = `${breed}\t${subBreed}`
+        }
+        else(breedNode.textContent = breed)
+        dogContNode.append(breedNode)
+    }  
+})
+console.log(dropDown.value)
+dropDown.addEventListener("change",(e)=> {
+    const listNode = document.querySelectorAll("li")
+    listNode.forEach(item=>{
+        if (item.textContent.slice(0,1) !== e.target.value) {
+            item.hidden = true;
+        }
+        else (item.hidden = false)
+
+    })
+})
+
+/**/
